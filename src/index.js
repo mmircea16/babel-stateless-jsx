6dbcddba8d81c2358858114ccdc2e410faf322bc
Mircea M <mircea.moise@gmail.com>
2018-01-12 16:43:41 +0000

    Small refactoring

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ module.exports = function ({types: t}) {
     let pathToTemplate = filePath.resolve(__dirname, 'component-template.jsx');
     let classTemplate = fs.readFileSync(pathToTemplate, 'utf-8');
 
-    const buildRequire = template(classTemplate, {sourceType: 'module'});
+    const templateFor = template(classTemplate, {sourceType: 'module'});
 
     return {
         name: 'statless-jsx',
         inherits: require("babel-plugin-syntax-jsx"),
         visitor: {
-            ExpressionStatement(path) {
-                const ast = buildRequire({
-                    SOURCE: path
+            ExpressionStatement(nodePath) {
+                const ast = templateFor({
+                    SOURCE: nodePath
                 });
 
-                path.replaceWithMultiple(ast);
+                nodePath.replaceWithMultiple(ast);
             }
         }
     }
