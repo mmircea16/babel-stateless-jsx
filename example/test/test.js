edc6d5a1f366040c549c6c552ef15cf203d7b84c
Mircea M <mircea.moise@gmail.com>
2018-02-11 14:00:10 +0000

    Added more coverage to the example

--- example/test/test.js
+++ example/test/test.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-import Hello from '../src/Hello.html.jsx'
+import SimpleHello from '../src/SimpleHello.html.jsx'
+import HelloWithProps from '../src/HelloWithProps.html.jsx'
 
 it('should render a hello world div', () => {
-    let test = <Hello name="World"/>;
+    let test = <SimpleHello/>;
+
+    let result = ReactDOMServer.renderToStaticMarkup(test);
+
+    expect(result).toBe("<div>Hello World!</div>")
+});
+
+
+it('should render a hello world div with the name coming from props', () => {
+    let test = <HelloWithProps name="World"/>;
 
     let result = ReactDOMServer.renderToStaticMarkup(test);
 
