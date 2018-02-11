d89654db3cc3015814804498b3639c558bc06cf4
Mircea M <mircea.moise@gmail.com>
2018-02-11 13:49:46 +0000

    Adding an example project to be used to test the end-to-end workings of the plugin

---
+++ example/test/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import Hello from '../src/Hello.html.jsx'
+
+it('should render a hello world div', () => {
+    let test = <Hello name="World"/>;
+
+    let result = ReactDOMServer.renderToStaticMarkup(test);
+
+    expect(result).toBe("<div>Hello World!</div>")
+});
