4af5a14a2aab1f4486dd5404716b0c9230fcb166
Mircea M <mircea.moise@gmail.com>
2018-02-11 14:03:47 +0000

    Added a more complex component test case

---
+++ example/src/HelloWithDOMChildren.html.jsx
@@ -0,0 +1,4 @@
+<div>
+    <p>Hello {this.props.name}!</p>
+    <p>This is just another paragraph</p>
+</div>
--- example/test/test.js
+++ example/test/test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import SimpleHello from '../src/SimpleHello.html.jsx'
 import HelloWithProps from '../src/HelloWithProps.html.jsx'
+import HelloWithDOMChildren from '../src/HelloWithDOMChildren.html.jsx'
 
 it('should render a hello world div', () => {
     let test = <SimpleHello/>;
@@ -18,4 +19,13 @@ it('should render a hello world div with the name coming from props', () => {
     let result = ReactDOMServer.renderToStaticMarkup(test);
 
     expect(result).toBe("<div>Hello World!</div>")
+});
+
+
+it('should render a hello world div with a bit more complex structure', () => {
+    let test = <HelloWithDOMChildren name="World"/>;
+
+    let result = ReactDOMServer.renderToStaticMarkup(test);
+
+    expect(result).toBe("<div><p>Hello World!</p><p>This is just another paragraph</p></div>")
 });
