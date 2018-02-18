b8554621f39c1698eb31646c05ce36a4c0eaa24d
Mircea M <mircea.moise@gmail.com>
2018-02-18 16:22:48 +0000

    Removing the __jsxpath attribute used to point to the source

--- src/visitors/componentsRetriever.js
+++ src/visitors/componentsRetriever.js
@@ -9,7 +9,18 @@ module.exports = {
             const jsxPaths = R.filter((node) => node.name.name === '__jsxpath', attrs);
             const source = jsxPaths.length === 0 ? "." : jsxPaths[0].value.value;
 
-            this.components.push(new Import(nodeName, source))
+            this.components.push(new Import(nodeName, source));
+
+            if (jsxPaths.length > 0) {
+                nodePath.traverse({
+                    JSXAttribute(nodePath) {
+                        let name = nodePath.node.name.name;
+                        if (name === '__jsxpath') {
+                            nodePath.remove()
+                        }
+                    }
+                })
+            }
         }
     }
 
--- test/visitors/componentsRetriever.test.js
+++ test/visitors/componentsRetriever.test.js
@@ -3,6 +3,17 @@ const componentRetriever = require("../../src/visitors/componentsRetriever");
 const traverse = require("babel-traverse");
 const Import = require("../../src/import").Import;
 
+function expectNoAttribute(ast, attributeName) {
+    traverse.default(ast, {
+        JSXAttribute(nodePath) {
+            let name = nodePath.node.name.name;
+            if (name === attributeName) {
+                expect(false).toBe(true);
+            }
+        }
+    })
+}
+
 it('contains import for Component found', () => {
     const {ast} = babel.transform("<div><Hola/><Hola><AnotherSaying/></Hola><Something/></div>",
         {plugins: ["@babel/plugin-syntax-jsx"]});
@@ -24,4 +35,12 @@ it('contains import for Component found', () => {
     expect(components).toEqual([
         new Import("Something", "somewhere"),
     ])
+});
+
+it('should remove the __jsxpath attribute', () => {
+    const {ast} = babel.transform("<div><Something __jsxpath='somewhere'/></div>",
+        {plugins: ["@babel/plugin-syntax-jsx"]});
+    let components = [];
+    traverse.default(ast, componentRetriever, null, {components});
+    expectNoAttribute(ast, "__jsxpath")
 });
